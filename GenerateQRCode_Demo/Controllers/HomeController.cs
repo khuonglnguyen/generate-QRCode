c7f59a5484f880ef93f6cb094b2a7c3c609199f1
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2022-09-26 17:20:33 +0700

    Add project files.

---
+++ GenerateQRCode_Demo/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using GenerateQRCode_Demo.Models;
+using IronBarCode;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace GenerateQRCode_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWebHostEnvironment _environment;
+        public HomeController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+        public IActionResult CreateQRCode()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateQRCode(GenerateQRCodeModel generateQRCode)
+        {
+            try
+            {
+                GeneratedBarcode barcode = QRCodeWriter.CreateQrCode(generateQRCode.QRCodeText, 200);
+                barcode.AddBarcodeValueTextBelowBarcode();
+                // Styling a QR code and adding annotation text
+                barcode.SetMargins(10);
+                barcode.ChangeBarCodeColor(Color.BlueViolet);
+                string path = Path.Combine(_environment.WebRootPath, "GeneratedQRCode");
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                string filePath = Path.Combine(_environment.WebRootPath, "GeneratedQRCode/qrcode.png");
+                barcode.SaveAsPng(filePath);
+                string fileName = Path.GetFileName(filePath);
+                string imageUrl = $"{this.Request.Scheme}://{this.Request.Host}{this.Request.PathBase}" + "/GeneratedQRCode/" + fileName;
+                ViewBag.QrCodeUri = imageUrl;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return View();
+        }
+    }
+}
---
+++ GenerateQRCode_Demo/Models/GenerateQRCodeModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GenerateQRCode_Demo.Models
+{
+    public class GenerateQRCodeModel
+    {
+        [Display(Name = "Enter QR Code Text")]
+        public string QRCodeText
+        {
+            get;
+            set;
+        }
+    }
+}
